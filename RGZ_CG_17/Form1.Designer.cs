f627e19d057da9566921251792577fa9256130bb
RockStar <kuhtin.vasya@yandex.ru>
2018-04-30 01:37:58 +0700

    Add form event listners

--- RGZ_CG_17/Form1.Designer.cs
+++ RGZ_CG_17/Form1.Designer.cs
@@ -31,16 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
             this.GLControl = new OpenTK.GLControl();
             this.main_panel = new System.Windows.Forms.Panel();
-            this.accuracyVaslue_label = new System.Windows.Forms.Label();
-            this.accuracyName_label = new System.Windows.Forms.Label();
-            this.accuracyValue_trackBar = new System.Windows.Forms.TrackBar();
             this.settings_groupBox = new System.Windows.Forms.GroupBox();
-            this.spline_checkBox = new System.Windows.Forms.CheckBox();
             this.primitive_checkBox = new System.Windows.Forms.CheckBox();
             this.derivative_checkBox = new System.Windows.Forms.CheckBox();
+            this.spline_checkBox = new System.Windows.Forms.CheckBox();
+            this.accuracyValue_trackBar = new System.Windows.Forms.TrackBar();
+            this.accuracyValue_label = new System.Windows.Forms.Label();
+            this.accuracyName_label = new System.Windows.Forms.Label();
             this.main_panel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.accuracyValue_trackBar)).BeginInit();
             this.settings_groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.accuracyValue_trackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // GLControl
@@ -51,44 +51,23 @@
             this.GLControl.Size = new System.Drawing.Size(807, 454);
             this.GLControl.TabIndex = 0;
             this.GLControl.VSync = false;
+            this.GLControl.Load += new System.EventHandler(this.GLControl_Load);
+            this.GLControl.Paint += new System.Windows.Forms.PaintEventHandler(this.GLControl_Paint);
+            this.GLControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GLControl_MouseDown);
+            this.GLControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GLControl_MouseMove);
+            this.GLControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.GLControl_MouseUp);
+            this.GLControl.Resize += new System.EventHandler(this.GLControl_Resize);
             // 
             // main_panel
             // 
             this.main_panel.Controls.Add(this.settings_groupBox);
             this.main_panel.Controls.Add(this.accuracyValue_trackBar);
-            this.main_panel.Controls.Add(this.accuracyVaslue_label);
+            this.main_panel.Controls.Add(this.accuracyValue_label);
             this.main_panel.Controls.Add(this.accuracyName_label);
             this.main_panel.Location = new System.Drawing.Point(1, -2);
             this.main_panel.Name = "main_panel";
             this.main_panel.Size = new System.Drawing.Size(86, 155);
             this.main_panel.TabIndex = 1;
-            // 
-            // accuracyVaslue_label
-            // 
-            this.accuracyVaslue_label.AutoSize = true;
-            this.accuracyVaslue_label.Location = new System.Drawing.Point(64, 11);
-            this.accuracyVaslue_label.Name = "accuracyVaslue_label";
-            this.accuracyVaslue_label.Size = new System.Drawing.Size(13, 13);
-            this.accuracyVaslue_label.TabIndex = 3;
-            this.accuracyVaslue_label.Text = "7";
-            // 
-            // accuracyName_label
-            // 
-            this.accuracyName_label.AutoSize = true;
-            this.accuracyName_label.Location = new System.Drawing.Point(0, 11);
-            this.accuracyName_label.Name = "accuracyName_label";
-            this.accuracyName_label.Size = new System.Drawing.Size(55, 13);
-            this.accuracyName_label.TabIndex = 2;
-            this.accuracyName_label.Text = "Accuracy:";
-            // 
-            // accuracyValue_trackBar
-            // 
-            this.accuracyValue_trackBar.Location = new System.Drawing.Point(3, 27);
-            this.accuracyValue_trackBar.Maximum = 40;
-            this.accuracyValue_trackBar.Name = "accuracyValue_trackBar";
-            this.accuracyValue_trackBar.Size = new System.Drawing.Size(82, 45);
-            this.accuracyValue_trackBar.TabIndex = 2;
-            this.accuracyValue_trackBar.Value = 7;
             // 
             // settings_groupBox
             // 
@@ -102,16 +81,6 @@
             this.settings_groupBox.TabStop = false;
             this.settings_groupBox.Text = "Settings";
             // 
-            // spline_checkBox
-            // 
-            this.spline_checkBox.AutoSize = true;
-            this.spline_checkBox.Location = new System.Drawing.Point(9, 19);
-            this.spline_checkBox.Name = "spline_checkBox";
-            this.spline_checkBox.Size = new System.Drawing.Size(55, 17);
-            this.spline_checkBox.TabIndex = 2;
-            this.spline_checkBox.Text = "Spline";
-            this.spline_checkBox.UseVisualStyleBackColor = true;
-            // 
             // primitive_checkBox
             // 
             this.primitive_checkBox.AutoSize = true;
@@ -121,6 +90,7 @@
             this.primitive_checkBox.TabIndex = 3;
             this.primitive_checkBox.Text = "Primitive";
             this.primitive_checkBox.UseVisualStyleBackColor = true;
+            this.primitive_checkBox.CheckedChanged += new System.EventHandler(this.spline_checkBox_CheckedChanged);
             // 
             // derivative_checkBox
             // 
@@ -131,6 +101,46 @@
             this.derivative_checkBox.TabIndex = 4;
             this.derivative_checkBox.Text = "Derivative";
             this.derivative_checkBox.UseVisualStyleBackColor = true;
+            this.derivative_checkBox.CheckedChanged += new System.EventHandler(this.spline_checkBox_CheckedChanged);
+            // 
+            // spline_checkBox
+            // 
+            this.spline_checkBox.AutoSize = true;
+            this.spline_checkBox.Location = new System.Drawing.Point(9, 19);
+            this.spline_checkBox.Name = "spline_checkBox";
+            this.spline_checkBox.Size = new System.Drawing.Size(55, 17);
+            this.spline_checkBox.TabIndex = 2;
+            this.spline_checkBox.Text = "Spline";
+            this.spline_checkBox.UseVisualStyleBackColor = true;
+            this.spline_checkBox.CheckedChanged += new System.EventHandler(this.spline_checkBox_CheckedChanged);
+            // 
+            // accuracyValue_trackBar
+            // 
+            this.accuracyValue_trackBar.Location = new System.Drawing.Point(3, 27);
+            this.accuracyValue_trackBar.Maximum = 40;
+            this.accuracyValue_trackBar.Name = "accuracyValue_trackBar";
+            this.accuracyValue_trackBar.Size = new System.Drawing.Size(82, 45);
+            this.accuracyValue_trackBar.TabIndex = 2;
+            this.accuracyValue_trackBar.Value = 7;
+            this.accuracyValue_trackBar.Scroll += new System.EventHandler(this.accuracyValue_trackBar_Scroll);
+            // 
+            // accuracyValue_label
+            // 
+            this.accuracyValue_label.AutoSize = true;
+            this.accuracyValue_label.Location = new System.Drawing.Point(64, 11);
+            this.accuracyValue_label.Name = "accuracyValue_label";
+            this.accuracyValue_label.Size = new System.Drawing.Size(13, 13);
+            this.accuracyValue_label.TabIndex = 3;
+            this.accuracyValue_label.Text = "7";
+            // 
+            // accuracyName_label
+            // 
+            this.accuracyName_label.AutoSize = true;
+            this.accuracyName_label.Location = new System.Drawing.Point(0, 11);
+            this.accuracyName_label.Name = "accuracyName_label";
+            this.accuracyName_label.Size = new System.Drawing.Size(55, 13);
+            this.accuracyName_label.TabIndex = 2;
+            this.accuracyName_label.Text = "Accuracy:";
             // 
             // Form
             // 
@@ -144,9 +154,9 @@
             this.Text = "V17";
             this.main_panel.ResumeLayout(false);
             this.main_panel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.accuracyValue_trackBar)).EndInit();
             this.settings_groupBox.ResumeLayout(false);
             this.settings_groupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.accuracyValue_trackBar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -155,7 +165,7 @@
 
         private OpenTK.GLControl GLControl;
         private System.Windows.Forms.Panel main_panel;
-        private System.Windows.Forms.Label accuracyVaslue_label;
+        private System.Windows.Forms.Label accuracyValue_label;
         private System.Windows.Forms.Label accuracyName_label;
         private System.Windows.Forms.GroupBox settings_groupBox;
         private System.Windows.Forms.CheckBox primitive_checkBox;
--- RGZ_CG_17/Form1.cs
+++ RGZ_CG_17/Form1.cs
@@ -1,20 +1,125 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenTK;
 
 namespace RGZ_CG_17
 {
     public partial class Form : System.Windows.Forms.Form
     {
+        private bool _initialized = false;
+        private Renderer r;
+        private Spline s;
+        private Point _mouse1, _mouse2;
+        private bool _isMoving = false;
+
         public Form()
         {
             InitializeComponent();
+            r = new Renderer(GLC);
+            s = new Spline();
+        }
+
+        private void GLControl_Resize(object sender, EventArgs e)
+        {
+            if (!_initialized) return;
+            r.ApplyResize();
+        }
+
+        private void GLControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Right:
+                    {
+                        _isMoving = true;
+                        _mouse1 = e.Location;
+                    }
+                    break;
+                case MouseButtons.Left:
+                    {
+                        _mouse2 = e.Location;
+                    }
+                    break;
+            }
+        }
+
+        private void GLControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Right: {
+                if (_isMoving)
+                {
+                    if (Math.Abs(_mouse1.X - e.X) > r.Scale || Math.Abs(_mouse1.Y - e.Y) > r.Scale)
+                    {
+                        r.Move(_mouse1.X - e.X, _mouse1.Y - e.Y);
+                        _mouse1 = e.Location;
+                        r.Refresh();
+                    }
+                }
+            }
+            break;
+                case MouseButtons.Left: {
+                r.Refresh();
+            }
+            break;
+        }
+    }
+
+        private void GLControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Right:
+                    {
+                        _isMoving = false;
+                    }
+                    break;
+                case MouseButtons.Left:
+                    {
+                        var pt = new PointF(e.X / r.Scale - r.TranslateX, -e.Y / r.Scale - r.TranslateY);
+                        s.AddPoint(pt);
+                        r.Refresh();
+                    }
+                    break;
+            }
+        }
+
+        private void GLControl_Load(object sender, EventArgs e)
+        {
+            ((GLControl)sender).MakeCurrent();
+            r.Initialize();
+
+            _initialized = true;
+        }
+
+        private void accuracyValue_trackBar_Scroll(object sender, EventArgs e)
+        {
+            accuracyValue_label.Text = accuracyValue_trackBar.Value.ToString();
+            s.Steps = accuracyValue_trackBar.Value;
+            r.Refresh();
+        }
+
+        private void spline_checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            r.DrawMode ^= (1 << Convert.ToInt32(((CheckBox)sender).Tag));
+            r.Refresh();
+        }
+
+        private void GLControl_Paint(object sender, PaintEventArgs e)
+        {
+            if (!_initialized) return;
+            ((GLControl)sender).MakeCurrent();
+
+            r.Begin();
+            r.DrawGrid();
+            r.DrawAxis();
+            r.DrawSpline(s);
+            r.DrawPoints(s);
+            r.End();
+
+            ((GLControl)sender).SwapBuffers();
         }
     }
 }
